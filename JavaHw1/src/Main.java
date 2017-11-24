84f34bfde0f6511be8d3bbc3aa8f465e71b3f94c
tlist888 <335071aA>
2017-11-25 00:21:07 +0300

    Fix bug

--- JavaHw1/src/Main.java
+++ JavaHw1/src/Main.java
@@ -1,15 +1,11 @@
 
 public class Main {
     public static void main(String []args){
-        Line line1 = new Line(new Point(1, 2), new Point(3, 4));
-        Line line2 = new Line(new Point(-1, -2), new Point(-3, -4));
-        Line line3 = new Line(new Point(1, 1), new Point(10, 10));
+        Line line1 = new Line(new Point(1, 1), new Point(2, 2));
+        Line line2 = new Line(new Point(-3, 0), new Point(1, 1));
 
-        System.out.println(line1.getLength());
-        System.out.println(line2.getLength());
-        System.out.println(line3.getLength());
+        if(line1.isEqualLength(line2)) System.out.println("This line is equal.");
+        else System.out.println("This line is not equal.");
 
-        System.out.println(line1.isEqualLength(line2));
-        System.out.println(line1.isEqualLength(line3));
     }
 }
